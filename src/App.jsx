79bedda1ca1207de4a4cb1c90355899025535bda
Harper <eunbyul1866@gmail.com>
2024-09-29 18:31:58 +0900

    Setup global state management with Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,94 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { useReducer, useRef, createContext } from "react";
 import Diary from "./pages/Diary";
 import Home from "./pages/Home";
 import New from "./pages/New";
+import Edit from "./pages/Edit";
 import NotFound from "./pages/NotFound";
 import Button from "./components/Button";
-import Header from "./components/Header";
+
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1 diary",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2 diary",
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+    case "DELETE":
+      return state.filter((item) => String(item.id) !== String(action.data.id));
+  }
+  return state;
+}
+
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
 
 function App() {
+  const idRef = useRef(3);
+  const [data, dispatch] = useReducer(reducer, mockData);
+
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      data: {
+        id,
+      },
+    });
+  };
   return (
     <>
-      <Header
-        title={"My Diary"}
-        leftChild={<Button text={"left"} />}
-        rightChild={<Button text={"right"} />}
-      />
-      <Button
-        text={"1234"}
-        type={"GOOD"}
-        onClick={() => {
-          console.log("click");
-        }}
-      />
-      <Button
-        text={"1234"}
-        type={"SOSO"}
-        onClick={() => {
-          console.log("click");
-        }}
-      />
-      <Button
-        text={"1234"}
-        type={"BAD"}
-        onClick={() => {
-          console.log("click");
-        }}
-      />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/diary/:id" element={<Diary />} />
-        <Route path="/add-diary" element={<New />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider value={{ onCreate, onDelete, onUpdate }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/add/diary" element={<New />} />
+            <Route path="/edit/diary/:id" element={<Edit />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
   );
 }
