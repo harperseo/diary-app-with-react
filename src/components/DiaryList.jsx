20c57cd8f9b176c19021ac2a3300a121c562ed7f
Harper <eunbyul1866@gmail.com>
2024-12-15 18:35:58 +0900

    Add sort data feature

--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -1,19 +1,37 @@
 import Button from "./Button";
 import DiaryItem from "./DiaryItem";
 import "./DiaryList.css";
+import { useState } from "react";
 
 const DiaryList = ({ data }) => {
+  const [sortType, setSortType] = useState("latest");
+
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value);
+  };
+
+  const getSortedData = () => {
+    return data.toSorted((a, b) => {
+      if (sortType === "oldest") {
+        return Number(a.createdDate) - Number(b.createdDate);
+      } else {
+        return Number(b.createdDate) - Number(a.createdDate);
+      }
+    });
+  };
+  const sortedData = getSortedData();
+
   return (
     <div className="DiaryList">
       <div className="menu_bar">
-        <select>
+        <select onChange={onChangeSortType}>
           <option value={"latest"}>Newest</option>
           <option value={"oldest"}>Oldest</option>
         </select>
         <Button text={"Add new diary"} type={"GOOD"} />
       </div>
       <div className="list_wrapper">
-        {data.map((item) => (
+        {sortedData.map((item) => (
           <DiaryItem key={item.id} {...item} />
         ))}
       </div>
